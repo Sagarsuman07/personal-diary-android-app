1d6a431cad5749a5c6650273e9332955d9085fa6
sagar suman <sagarsumanrathore@gmail.com>
2024-08-30 19:09:00 +0530

    stable dashboard image and scroll view version 22

--- app/src/main/java/com/mitwpu/posdiary/dashboard.java
+++ app/src/main/java/com/mitwpu/posdiary/dashboard.java
@@ -1,6 +1,7 @@
 package com.mitwpu.posdiary;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +18,14 @@ public class dashboard extends AppCompatActivity {
 
         setContentView(R.layout.activity_dashboard);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
 
     }
 }
